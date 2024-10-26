b8e578bc304fe8553a487a9a8132e497e40924ed
Sairathna123 <143626449+Sairathna123@users.noreply.github.com>
2024-10-26 19:24:57 +0530

    Delete light-dark-mode.js

--- light-dark-mode.js
+++
@@ -1,8 +0,0 @@
-// light-dark-mode.js - Handle light/dark mode toggle
-
-const toggleSwitch = document.getElementById("themeSwitcher");
-
-toggleSwitch.addEventListener("change", function () {
-    document.body.classList.toggle("dark-mode");
-    // Optionally save user preference in localStorage
-});
